bdd0e242e726062af1f8c2652d1c2e1b93d6eb47
matt319940 <matt319940@yahoo.com>
2019-11-17 18:50:42 -0500

    Bare bones working

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,6 +3,35 @@ var burger = require("../models/burger.js");
 
 var app = express();
 
-app.get("");
+
+
+app.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render('index', hbsObject);
+    });
+});
+
+app.post("/burgers", function (req, res) {
+    burger.insertOne([
+        "burger_name"
+    ], [
+        req.body.burger_name
+    ], function (data) {
+        res.redirect('/');
+    });
+});
+
+app.post('/burgers/:id', function (req, res) {
+    var condition = 'id = ' + req.params.id;
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function (data) {
+        res.redirect('/');
+    });
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-var expresshdbs = require("express-handlebars");
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+app.use(express.static("public"));
+
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+var rout = require("./controllers/burgers_controllers.js");
+app.use("/", rout);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
